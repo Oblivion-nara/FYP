3065be3fb66e4b70b3d40d0fcc701a98d88036d3
Reece M <reecemeek@gmail.com>
2017-11-06 11:58:25 +0000

    Added the mokapot demos and jar file

---
+++ src/main/PalindromeVerifier.java
@@ -0,0 +1,14 @@
+package main;
+
+import xyz.acygn.mokapot.NonCopiable;
+
+/**
+ * @author Kelsey McKenna
+ */
+public class PalindromeVerifier implements NonCopiable {
+
+    boolean verify(String s) {
+        return s.equals(new StringBuilder(s).reverse().toString());
+    }
+
+}
